ca34e2b6b6496d02bea11a40a98cbc3e09075bae
Aki146 <b30767@gmail.com>
2024-05-12 20:55:50 +0800

    feat：【HDAC】改成了postfix的方案

---
+++ RimWorldTestMod/src/HarmonyPatches/Verse_VerbUtility_UsesExplosiveProjectiles.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace HDAC.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Verse.VerbUtility), "UsesExplosiveProjectiles")]
+    public class Verse_VerbUtility_UsesExplosiveProjectiles
+    {
+        public static void Postfix(ref bool __result, Verb verb)
+        {
+            if (verb.caster is Pawn pawn)
+            {
+                var primary = pawn.equipment.Primary;
+                if (primary != null)
+                {
+                    var comp = primary.TryGetComp<Comp_AutoCannon>();
+                    if (comp != null)
+                    {
+                        __result = comp.curMode == Mode.Common;
+                    }
+                }
+            }
+        }
+    }
+}
